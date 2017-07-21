41baba02fe267fd10eedeab89889ba4631ba6d18
Matheus Mauricio <matheus_mauricio@hotmail.com>
2017-07-21 13:50:52 -0300

    Commit 21/07

--- Códigos/OuveFacil (servidor - colocar na pasta do wamp ou xampp)/login.php
+++ Códigos/OuveFacil (servidor - colocar na pasta do wamp ou xampp)/login.php
@@ -4,10 +4,10 @@ include_once("conexao.php");
 
 if ($_POST) {
     $login = $_POST['login'];
-    //$senha = md5($_POST['senha']);
+    $senha = md5($_POST['senha']);
 
     $query = "SELECT usuario.nome, usuario.login, usuario.senha, usuario.cpfCnpj FROM `bancoOuveFacil`.`usuario`
-    WHERE usuario.login = '$login'";
+    WHERE usuario.login = '$login' AND usuario.senha = '$senha'";
     $queryExec = mysqli_query($con, $query) or die ("Erro: " .mysqli_error($con));
 
     while ($linha = mysqli_fetch_assoc($queryExec)) {
--- Códigos/OuveFacil (servidor - colocar na pasta do wamp ou xampp)/relatorioAvaliacaoBairro2.php
+++ Códigos/OuveFacil (servidor - colocar na pasta do wamp ou xampp)/relatorioAvaliacaoBairro2.php
@@ -3,14 +3,11 @@
   include_once("conexao.php");
 
 if ($_POST) {
-  $status =  $_POST['status'];
   $bairro = $_POST['bairro'];
 
-
   $query = "SELECT COUNT(*)
-  FROM `bancoOuveFacil`.`denuncia`, `bancoOuveFacil`.`status`, `bancoOuveFacil`.`bairro`
-  WHERE denuncia.codStatus = status.codStatus AND status.nome = '$status' AND denuncia.codBairro = bairro.codBairro
-  AND bairro.nome = '$bairro'";
+  FROM `bancoOuveFacil`.`denuncia`, `bancoOuveFacil`.`bairro`
+  WHERE denuncia.codBairro = bairro.codBairro AND bairro.nome = '$bairro'";
 
   $queryExec = mysqli_query($con, $query) or die ("Erro: " .mysqli_error($con));
 
